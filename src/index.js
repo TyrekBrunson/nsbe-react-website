dbe83c45979e374d92a6a62356ad1ee77ea9cdf9
TyrekBrunson <tyrekbrunson@aol.com>
2024-11-04 16:28:34 -0500

    changed basename

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import './style.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
